6ab40504f4db6ab1d8f651a63bd4b35f4e29c9c2
lucas vinicius <lucassilved@gmail.com>
2024-08-30 07:43:46 -0300

    feat: add task routes

---
+++ src/infrastructure/express/routes/task-routes.ts
@@ -0,0 +1,26 @@
+import {
+  makeCreateTaskController,
+  makeDeleteTaskController,
+  makeGetTaskController,
+  makeListTaskController,
+  makeUpdateTaskController,
+} from '@/main/factories/task-factory';
+import type { Handler, Router } from 'express';
+
+export function taskRoutes(router: Router, authentication: Handler) {
+  router.post('/task', authentication, (request, response) =>
+    makeCreateTaskController().handle(request, response),
+  );
+  router.put('/task/:id', authentication, (request, response) =>
+    makeUpdateTaskController().handle(request, response),
+  );
+  router.delete('/task/:id', authentication, (request, response) =>
+    makeDeleteTaskController().handle(request, response),
+  );
+  router.get('/task/:id', authentication, (request, response) =>
+    makeGetTaskController().handle(request, response),
+  );
+  router.get('/tasks', authentication, (request, response) =>
+    makeListTaskController().handle(request, response),
+  );
+}

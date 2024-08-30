e075031af75604c5c61cfc7c2ca1038007965951
lucas vinicius <lucassilved@gmail.com>
2024-08-30 07:39:00 -0300

    feat: add entries for pagination in task list

--- src/application/repositories/i-tasks-repository.ts
+++ src/application/repositories/i-tasks-repository.ts
@@ -1,13 +1,9 @@
 import { Task } from '@/domain/task';
 
 export interface ITasksRepository {
-  save(userId: string, data: Task): Promise<Task | undefined>;
-  update(data: Task): Promise<Task | undefined>;
+  save(userId: string, input: Task): Promise<Task | undefined>;
+  update(input: Task): Promise<Task | undefined>;
   delete(id: string): Promise<Task | undefined>;
   get(id: string): Promise<Task | undefined>;
-  getAll(
-    userId: string,
-    skip: number,
-    take: number,
-  ): Promise<Task[] | undefined>;
+  getAll(userId: string, skip: number, take: number): Promise<Task[]>;
 }
